21d9f1b4fecd533638cc4d85b6224b56e8d9df46
ymaprameya1~ <ymaprameya@gmail.com>
2025-06-24 23:56:09 +0530

    hello after load2

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ console.dir(heading);
 const p = document.createElement("p");
 p.textContent ="Hello after load";
 document.body.appendChild(p);
+const p1 = document.createElement("p");
+p1.textContent ="Hello after load";
+document.body.appendChild(p1);
